d1a9f67e8153b0d071a2ed9e14b19f203af9dc3d
programming10000 <dutycycle480@gmail.com>
2023-03-28 10:16:15 +0300

    multiple routes

--- app.js
+++ app.js
@@ -1,49 +1,28 @@
 const express=require('express');
 const app=express();
-const path=require('path'); // use to access the project outside of the
-                                // the project directory 
-
-// app.set("view engine","ejs")
-
-// const userRouter=require("./Routes/users")
-// app.use("/users",userRouter)
-
-// install pug or ejs to view the routed file of users in html page on the express server
-
-app.use(express.static(path.join(__dirname + '/public')))
-
-  /* node Express/app on terminal to run
-the file found in this directory is served if the folder is found inside
- the working dir or outsite if the path and the folder name is correct */
-
-// app.use((req,res) => {
-//     res.status(404);
-//     res.send('<h1>Error 404</h1>')
-// })
-
-
-
-// like this we can serve the static front end project by putting 
-// inside the public folder
 
 
 
 
-// //  we can also serve api request by localhost:3000/api
-//  app.get('/api',(req,res) =>{
-//     res.json('http get request recieved json file');
-//  })
+app.get("/",(req,res) =>{
+    res.send("user List");
+  });
 
+app.get("/animal",(req,res) =>{
+    res.send("animal List");
+  });
+  
+app.get("/students",(req,res) =>{
+    res.send("students List");
+  });
+  
+app.get("/cars",(req,res) =>{
+    res.send("cars List");
+  });
 
-/* add sttic or public folder accesing static files on express
- server if express is dirictlly installed in the project directiory  */
-// app.use(express.static('public'));
+// const carRoute=require("./routes/car");
+// app.use("/car",userRoute);
 
-// if we have multiple routes in this file then we can use routing by 
-// creating routse folder and create the spesific routing file in it and  declare that file as a variable 
-// in this file and access multiple routing fum=nctionality easlly
-
-
-app.listen(3000,() =>{                       // it is used to run the application on port 300 it is visible on the terminal
+app.listen(3000,() =>{                       
     console.log('app listen on port 3000')
 })
