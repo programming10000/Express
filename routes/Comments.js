04ae636a4dbc698bdf5bf451bd7c1e4849eed881
programming10000 <dutycycle480@gmail.com>
2023-03-28 12:20:13 +0300

    user updated from json to normal

--- routes/Comments.js
+++ routes/Comments.js
@@ -2,6 +2,17 @@ const express=require("express");
 const router=express.Router();
 
 router.get("/",(req,res)=>{
-    res.send({data:"Here Is yor data"});
+    res.send({data:"Here Is yor comments"});
+});
+router.post("/",(req,res)=>{
+    res.send({data:"User created"});
+});
+
+router.put("/",(req,res)=>{
+    res.send({data:"User Updated"});
+});
+
+router.delete("/",(req,res)=>{
+    res.send({data:"user deleted"});
 });
 module.exports=router;
--- routes/User.js
+++ routes/User.js
@@ -3,6 +3,17 @@ const router=express.Router();
 
 
 router.get("/",(req,res)=>{
-    res.send({data:"Here Is yor data"});
+    res.send("Here Is yor data");
+});
+router.post("/",(req,res)=>{
+    res.send({data:"User created"});
+});
+
+router.put("/",(req,res)=>{
+    res.send({data:"User Updated"});
+});
+
+router.delete("/",(req,res)=>{
+    res.send({data:"user deleted"});
 });
 module.exports=router;
